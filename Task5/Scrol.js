d1b5b1f8ed981c03bdb022784762297a5f6973d9
Keerthiga5 <37925271+Keerthiga5@users.noreply.github.com>
2019-07-08 09:49:20 +0530

    updated scrol.js

--- Task5/Scrol.js
+++ Task5/Scrol.js
@@ -13,12 +13,14 @@
 	var timer=1;
 	var count=0;
 
+
 	for(let k=1;k<=5;k++)
 	{
 		append();
 	}
 
 	contBottom=elem.offsetHeight;
+	page=document.getElementById('pageno').value;
 
 	// function throttle (func, interval) {
  // 		var timeout;
@@ -35,26 +37,26 @@
  //  		}
 	//}
 
-	function scrollHeavyfunc()
-	{
-		console.log(j);
-		down();
-		up();
-		pos = elem.scrollTop;
-		pval=j-60;
-		var divs=elem.childNodes;
-		for(i=0;i<divs.length;i++)
-		{
-			var thisTop=(divs[i].getBoundingClientRect().top);
-			var h=parseInt(divs[i].style.height);
-			console.log(h);
-			if(thisTop>=contTop && h+thisTop<=contBottom+500)
-			{
-				document.getElementById('pageno').value=divs[i].id;
-				break;
-			}
-		}
-	}
+	// function scrollHeavyfunc()
+	// {
+	// 	console.log(j);
+	// 	down();
+	// 	up();
+	// 	pos = elem.scrollTop;
+	// 	pval=j-60;
+	// 	var divs=elem.childNodes;
+	// 	for(i=0;i<divs.length;i++)
+	// 	{
+	// 		var thisTop=(divs[i].getBoundingClientRect().top);
+	// 		var h=parseInt(divs[i].style.height);
+	// 		console.log(h);
+	// 		if(thisTop>=contTop && h+thisTop<=contBottom+500)
+	// 		{
+	// 			document.getElementById('pageno').value=divs[i].id;
+	// 			break;
+	// 		}
+	// 	}
+	// }
 
 	// const scrollCheckFunc=function()
 	// {
@@ -72,7 +74,6 @@
 
 	elem.addEventListener("scroll", function()
 	{
-		console.log(j);
 		down();
 		up();
 		pos = elem.scrollTop;
@@ -82,7 +83,6 @@
 		{
 			var thisTop=(divs[i].getBoundingClientRect().top);
 			var h=parseInt(divs[i].style.height);
-			console.log(h);
 			if(thisTop>=contTop && h+thisTop<=contBottom+500)
 			{
 				document.getElementById('pageno').value=divs[i].id;
@@ -145,7 +145,8 @@
 			for (i = 0; i < 10; i++) {
 				var str = map.get(ind);
 				var newbox = document.createElement("div");
-				newbox.setAttribute("style", "height:50px; text-align:center; width:300px; border: 1px solid black; margin-bottom: 3px; line-height: 50px;");
+				newbox.setAttribute("id","i"+ind);
+				newbox.classList.add('newbox_style');
 				newbox.innerHTML=ind + " : " + str;
 				newdiv.appendChild(newbox);
 				ind++;
@@ -162,7 +163,8 @@
 					var Str = db_txt[ind];
 					map.set(ind, Str);				
 					var newbox = document.createElement("div");
-					newbox.setAttribute("style", "height:50px; text-align:center; width:300px; border: 1px solid black; margin-bottom: 3px; line-height: 50px");
+					newbox.setAttribute("id","i"+ind);
+					newbox.classList.add('newbox_style');
 					newbox.innerHTML=ind + " : " + Str;
 					newdiv.appendChild(newbox);
 					ind++;
@@ -222,40 +224,88 @@
 		document.getElementById(page).scrollIntoView();
 	}
 
-	var debounce=function(func,wait,immediate)
-	{
-		var t;
-		return function(){
-			var context=this, args=arguments;
-			var later=function(){
-				t=null;
-				if(!immediate)
-				{
-					func.apply(context,args);
-				}
-			};
-			var callNow=immediate && !t;
-			clearTimeout(t);
-			t=setTimeout(later,wait);
-			if(callNow)
-			{
-				func.apply(context,args);
-			}
-		};
-	};
+	var debounce=function(func, interval) {
+  		var timeout;
+  		return function () {
+   			var context = this, args = arguments;
+    		var later = function () {
+      			timeout = null;
+      			func.apply(context, args);
+    		};
+    		clearTimeout(timeout);
+    		timeout = setTimeout(later, interval);
+  		}
+	}
 
-	document.getElementById('search').addEventListener('keyup',debounce(suggest,200));
+	var xhr=new XMLHttpRequest();
 
-	const suggest=function()
+	const suggest=function(e)
 	{
+		e.stopImmediatePropagation();
 		var txtToFind=document.getElementById('search').value;
-		
+		var list=document.getElementById('hugelist');
+		if(txtToFind.length<1)
+		{
+			return;
+		}
+		if(!isNaN(txtToFind))
+		{
+			alert("Entered a number ! Please enter Country name");
+			return;
+		}
 		//search from map--to be implemeted
+		if(txtToFind.length>0)
+		{
+			xhr.onreadystatechange=function(){
+			if(this.readyState==4 && this.status==200){
+				var response=JSON.parse(xhr.response);
+				while(list.firstChild)
+				{
+					list.removeChild(list.firstChild);
+				}
+				for(i=0;i<response.length;i++)
+				{
+					var option=document.createElement('option');
+					var idval=response[i].split(" : ");
+					option.setAttribute("data-value",parseInt(idval[0]));
+					option.innerText=idval[1];
+					list.appendChild(option);
+				}
+			}
+			};
+			xhr.open("GET","/Task5/load/SearchList?w="+txtToFind,false);
+			xhr.send();
+			document.querySelector('input[list]').addEventListener('keyup', function(e) {
+	    		var input = e.target;
+    	    	var list = input.getAttribute('list');
+       			var options = document.querySelectorAll('#' + list + ' option');
+        		var hiddenInput = document.getElementById(input.getAttribute('id') + '-hidden');
+	        	var label = input.value;
+				hiddenInput.value = label;
+    			for(var i = 0; i < options.length; i++) {
+        			var option = options[i];
+        			if(option.innerText === label) {
+            			hiddenInput.value = option.getAttribute('data-value');
+    					insertFivePages(((Math.ceil(hiddenInput.value/10)-3)*10)+1,Math.ceil(hiddenInput.value/10));
+    					document.getElementById("i"+hiddenInput.value).classList.add('hover');
+            			break;
+        			}
+    			}
+			});
+		}			
+		}
 
-		
+	document.getElementById('search').addEventListener('keyup',debounce(suggest,1000));
 
-
-
-	};
+	document.getElementById('pageno').addEventListener('keydown',function(e)
+	{
+		if(e.keyCode==13)
+		{
+			page=document.getElementById('pageno').value;
+			insertFivePages((page*10)-9,page);	
+		}
+	});
+	// window.util = {};
+	// window.util.debounce = debounce
 
 })();
